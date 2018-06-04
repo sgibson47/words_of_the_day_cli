b889f23df397067fecbbcbda577fb058b4eb132c
sgibson47 <sgibson47@gmail.com>
2018-06-04 12:12:10 -0500

    #list words uses word objects

--- lib/words_of_the_day/cli.rb
+++ lib/words_of_the_day/cli.rb
@@ -1,5 +1,42 @@
+require_relative './word.rb'
 class CLI
   def call
+    list_words
+    menu
+    goodbye
+  end
+
+  def list_words
+    #should eventually scrape & create word objects
     puts "Today's Words"
+    Word.all.each_with_index {|word, i|
+      puts "#{i +1}. #{word.name}"}
+    puts "\n"
+  end 
+
+  def menu
+    puts "Enter a number to learn more about the"
+    puts "corresponding word or enter exit to quit."
+
+    input = nil
+
+    while input != "exit"
+      case input = gets.strip
+      when "1"
+        puts "info on word 1"
+      when "2"
+        puts "info on word 2"
+      when "3"
+        puts "info on word 3"
+      when "exit"
+        break
+      else
+        puts "Please enter a number from the list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Bye! Come back tomorrow for new words!"
   end
 end
---
+++ lib/words_of_the_day/word.rb
@@ -0,0 +1,27 @@
+class Word
+  attr_accessor :name, :definition, :part_of_speech
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  word_1 = Word.new
+  word_1.name = "proficient"
+  word_1.definition = "well advanced in an art, occupation, or branch of knowledge"
+  word_1.part_of_speech = "adjective"
+  @@all<<word_1
+
+  word_2 = Word.new
+  word_2.name = "atavism"
+  word_2.definition = "reversion to an earlier type; throwback"
+  word_2.part_of_speech = "noun"
+  @@all<<word_2
+
+  word_3 = Word.new
+  word_3.name = "Precipitous"
+  word_3.definition = "Done rashly: acting too quickly and without sufficient thought"
+  word_3.part_of_speech = "adj"
+  @@all<<word_3
+end
